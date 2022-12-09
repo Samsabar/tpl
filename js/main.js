b3151bb2d5b1d81fda497ac3cf5c2fd94f5f2cd8
Михаил <85585993+Samsabar@users.noreply.github.com>
2022-12-09 11:52:30 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,3 +18,18 @@ $('.panel-collapse').on('shown.bs.collapse', function(e) {
     scrollTop: $panel.offset().top
   }, 700);
 });
+
+
+// Табы
+$(document).ready(function () {
+  let tabsItem = $('.tabs__item');
+  let contentItem = $('.content__item');
+
+  tabsItem.on("click", function (event){
+      let activeContent = $(this).attr("data-target");
+      tabsItem.removeClass("tabs__item--active");
+      contentItem.removeClass("content__item--active"); 
+      $(activeContent).toggleClass("content__item--active");
+      $(this).addClass("tabs__item--active")
+  });
+});
